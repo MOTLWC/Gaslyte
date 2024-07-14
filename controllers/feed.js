200c060cce071b601f85cda223467c3dd46aaf69
FelixMR <felixmcneillrutter@gmail.com>
2024-07-14 13:59:52 +0100

    Set up feed router imports/exports

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,9 +4,11 @@ const express = require("express");
 // Model Imports 
 
 // Controller Setup
+const router = express.Router();
 
 // Routes
 //! POST
 //! ADD
 
 // Export Module
+module.exports = router;
