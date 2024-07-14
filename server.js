af8d6a0662fa809b79ecc82b565e723634c73822
FelixMR <felixmcneillrutter@gmail.com>
2024-07-14 14:05:07 +0100

    Typo fix in Library Import section

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo");
 const dotenv = require("dotenv");
 const express = require("express");
-const expressServer = reqire("express-session");
+const expressServer = require("express-session");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
