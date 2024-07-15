43bd0a5af110a7682433fa29fa861159bb4e95b7
FelixMR <felixmcneillrutter@gmail.com>
2024-07-15 10:48:46 +0100

    Added post routes to auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,18 @@ const router = express.Router();
 router.get("/sign-in", (req, res) => {
     res.render("sign-in.ejs");
 });
+
 router.get("/sign-up", (req, res) => {
     res.render("/sign-up.ejs");
 });
 
+router.post("/sign-in", (req,res) => {
+
+});
+
+router.post("/sign-up", (req,res) => {
+    
+});
 
 // Export Module
 module.exports = router;
