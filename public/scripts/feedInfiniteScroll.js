56dd431e2ece20c130be290838294e4718038df5
FelixMR <felixmcneillrutter@gmail.com>
2024-07-18 13:27:51 +0100

    Added filter functionality

--- public/scripts/feedInfiniteScroll.js
+++ public/scripts/feedInfiniteScroll.js
@@ -3,10 +3,14 @@ const menuElements = document.getElementById("menuElements");
 const menuToggle = document.getElementById("menuToggle");
 const mainFeed = document.getElementById("mainFeed");
 
+const filterForm = document.getElementById("filterForm");
+
 menuToggle.addEventListener("click", toggleMenuElements);
+filterForm.addEventListener("submit", setFilters);
 
 const nextPosts = [];
 const shownPosts = [];
+const filterObject = {};
 
 async function sendUrlQuery() {
     try {
@@ -19,10 +23,19 @@ async function sendUrlQuery() {
 }
 
 async function getNextPosts(){
+    console.log(filterObject)
+}
 
+async function setFilters() {
+    const tempParams= new URLSearchParams(window.location.search);
+    tempParams.forEach((value, key) => {
+        filterObject[key] = value;
+    })
 }
 
 function toggleMenuElements() {
     console.log("running")
     menuElements.style.display = (menuElements.style.display === "flex")? "none" : "flex";
 }
+setFilters();
+getNextPosts()
